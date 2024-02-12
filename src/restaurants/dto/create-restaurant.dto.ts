1abeb253f1b26d2973c98add0a0d3d577908f781
MateusR <mateusftc_@hotmail.com>
2024-02-11 23:17:23 -0300

    feat(createUserDTO): Cass Validator aplicado

--- src/restaurants/dto/create-restaurant.dto.ts
+++ src/restaurants/dto/create-restaurant.dto.ts
@@ -1,15 +1,34 @@
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsPhoneNumber,
+  IsString,
+} from 'class-validator';
 import { Category } from '../schema/restaurant.schema';
 
 export class CreateRestaurantDTO {
+  @IsString()
+  @IsNotEmpty()
   readonly name: string;
 
+  @IsString()
+  @IsNotEmpty()
   readonly description: string;
 
+  @IsEmail()
+  @IsNotEmpty()
   readonly email: string;
 
+  @IsNotEmpty()
+  @IsPhoneNumber('BR')
   readonly phoneNumber: string;
 
+  @IsString()
+  @IsNotEmpty()
   readonly adress: string;
 
+  @IsNotEmpty()
+  @IsEnum(Category)
   readonly category: Category;
 }
