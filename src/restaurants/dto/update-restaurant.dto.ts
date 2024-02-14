deaac92d5b1a7c983c401f250727a0f5ab4c6304
MateusR <mateusftc_@hotmail.com>
2024-02-13 21:43:45 -0300

    feat(DTO): Feito o updateRestaurantDTO

--- src/restaurants/dto/update-restaurant.dto.ts
+++ src/restaurants/dto/update-restaurant.dto.ts
@@ -1,15 +1,34 @@
 import { Category } from '../schema/restaurant.schema';
+import {
+  IsEmail,
+  IsEnum,
+  IsOptional,
+  IsPhoneNumber,
+  IsString,
+} from 'class-validator';
 
 export class UpdateRestaurantDto {
+  @IsString()
+  @IsOptional()
   readonly name: string;
 
+  @IsString()
+  @IsOptional()
   readonly description: string;
 
+  @IsEmail({}, { message: 'Please enter a valid email address' })
+  @IsOptional()
   readonly email: string;
 
+  @IsPhoneNumber('BR')
+  @IsOptional()
   readonly phoneNumber: number;
 
-  readonly adress: string;
+  @IsString()
+  @IsOptional()
+  readonly address: string;
 
+  @IsEnum(Category, { message: 'Please enter correct category' })
+  @IsOptional()
   readonly category: Category;
 }
